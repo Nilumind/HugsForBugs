310aeeb3b6a1104f682f7cabb4731b2e216d4148
Chalana Prabhashwara <gmchalanaprabhashwara@gmail.com>
2023-06-08 11:10:10 +0530

    Create Welcome page

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Home.css';
+
+
+export default function Home() {
+  return (
+    
+      <div className='background-image'>
+          
+          <div className='but'>
+            <button type="submit" class="button" align="center" >Write Your<br/> To-Dos Here</button>
+          </div>
+
+
+
+      </div>
+      
+    
+    
+  )
+}
+
