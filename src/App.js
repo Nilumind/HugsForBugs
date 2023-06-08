458aa9947dc160aa579ffe16fbe116810a83bbdc
Chalana Prabhashwara <120502017+GMchalana@users.noreply.github.com>
2023-06-08 12:35:53 +0530

    Update App.js
    add the other page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Todo from './pages/Todo';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
-
+        <Route path="/todo" element={<Todo/>}/>
       </Routes>
     </Router>
   );
