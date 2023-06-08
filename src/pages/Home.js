bee28abb83afe8d0d44b6bd6333ac427a1b005b9
Chalana Prabhashwara <120502017+GMchalana@users.noreply.github.com>
2023-06-08 12:36:49 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Home.css';
-
+import {useNavigate} from 'react-router-dom';
 
 export default function Home() {
+  let navigate= useNavigate();
   return (
     
       <div className='background-image'>
           
           <div className='but'>
-            <button type="submit" class="button" align="center" >Write Your<br/> To-Dos Here</button>
+            <button type="submit" class="button" align="center" onClick={()=>{navigate("/Todo")}} >Write Your<br/> To-Dos Here</button>
           </div>
 
 
